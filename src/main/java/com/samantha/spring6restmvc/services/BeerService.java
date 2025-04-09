fc554bc313b0a55c905d1d0017f0331069dc6942
Ogundare Oluwafemi <ogundarearnold@gmail.com>
2025-04-09 14:20:52 +0100

    Created lombok Controller using the @Controller Annotation of lombok and added the @Service annotation to the Service layer

--- src/main/java/com/samantha/spring6restmvc/services/BeerService.java
+++ src/main/java/com/samantha/spring6restmvc/services/BeerService.java
@@ -1,9 +1,11 @@
 package com.samantha.spring6restmvc.services;
 
 import com.samantha.spring6restmvc.model.Beer;
+import org.springframework.stereotype.Service;
 
 import java.util.UUID;
 
+@Service
 public interface BeerService {
     Beer getBeerById(UUID id);
 }
