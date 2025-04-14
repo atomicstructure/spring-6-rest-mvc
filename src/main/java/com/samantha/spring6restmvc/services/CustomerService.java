4d4fec224d8a9f63d4b2bd110c2f673fb8dc71ae
Ogundare Oluwafemi <ogundarearnold@gmail.com>
2025-04-14 10:14:47 +0100

    Configured Mockito to write a mock test for the getBeerById class

--- src/main/java/com/samantha/spring6restmvc/services/CustomerService.java
+++ src/main/java/com/samantha/spring6restmvc/services/CustomerService.java
@@ -2,6 +2,7 @@ package com.samantha.spring6restmvc.services;
 
 
 import com.samantha.spring6restmvc.model.Customer;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.UUID;
@@ -9,6 +10,7 @@ import java.util.UUID;
 /**
  * Created by Samantha on 2023-10-01
  */
+@Service
 public interface CustomerService {
 
     List<Customer> listCustomers();
