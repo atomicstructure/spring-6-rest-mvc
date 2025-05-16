50135865662ae8b4211663a5102f14b842a1e9a2
Ogundare Oluwafemi <ogundarearnold@gmail.com>
2025-05-16 18:09:15 +0100

    JPA Validation

--- src/test/java/com/samantha/spring6restmvc/repositories/BeerRepositoryTest.java
+++ src/test/java/com/samantha/spring6restmvc/repositories/BeerRepositoryTest.java
@@ -1,9 +1,12 @@
 package com.samantha.spring6restmvc.repositories;
 
 import com.samantha.spring6restmvc.entity.Beer;
+import com.samantha.spring6restmvc.model.BeerStyle;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import java.math.BigDecimal;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
@@ -18,8 +21,13 @@ class BeerRepositoryTest {
     @Test
     void testSavedBeer() {
         Beer savedBeer = beerRepository.save(Beer.builder()
-                .beerName("Beer Name")
+                        .beerName("Beer Name")
+                        .beerStyle(BeerStyle.IPA)
+                        .upc("123456789012")
+                        .price(new BigDecimal("9.99"))
                 .build());
+
+        beerRepository.flush();
         assertThat(savedBeer).isNotNull();
         assertThat(savedBeer.getId()).isNotNull();
     }
