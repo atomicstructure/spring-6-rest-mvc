70752569a6b48f4c9730ef22dc9bfc359f5ac791
Ogundare Oluwafemi <ogundarearnold@gmail.com>
2025-05-28 12:30:22 +0100

    Fixed the Integration Tests

--- src/test/java/com/samantha/spring6restmvc/bootstrap/BootStrapDataTest.java
+++ src/test/java/com/samantha/spring6restmvc/bootstrap/BootStrapDataTest.java
@@ -3,15 +3,19 @@ package com.samantha.spring6restmvc.bootstrap;
 import com.samantha.spring6restmvc.repositories.BeerRepository;
 import com.samantha.spring6restmvc.repositories.CustomerRepository;
 import com.samantha.spring6restmvc.services.BeerCsvService;
+import com.samantha.spring6restmvc.services.BeerCsvServiceImpl;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.context.annotation.Import;
+
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
-import static org.junit.jupiter.api.Assertions.*;
+
 
 @DataJpaTest
+@Import(BeerCsvServiceImpl.class)
 class BootStrapDataTest {
 
     @Autowired
@@ -34,7 +38,7 @@ class BootStrapDataTest {
     void Testrun() throws Exception {
         bootStrapData.run(null);
 
-        assertThat(beerRepository.count()).isEqualTo(3);
+        assertThat(beerRepository.count()).isEqualTo(2413);
 
         assertThat(customerRepository.count()).isEqualTo(3);
     }
