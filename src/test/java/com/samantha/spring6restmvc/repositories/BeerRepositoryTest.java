08a4178aa42242d93b639fb27118746c4f9500ca
Ogundare Oluwafemi <ogundarearnold@gmail.com>
2025-06-02 14:42:56 +0100

    Add method to find beers by name with case-insensitive search in BeerRepository

--- src/test/java/com/samantha/spring6restmvc/repositories/BeerRepositoryTest.java
+++ src/test/java/com/samantha/spring6restmvc/repositories/BeerRepositoryTest.java
@@ -1,24 +1,35 @@
 package com.samantha.spring6restmvc.repositories;
 
+import com.samantha.spring6restmvc.bootstrap.BootStrapData;
 import com.samantha.spring6restmvc.entities.Beer;
 import com.samantha.spring6restmvc.model.BeerStyle;
+import com.samantha.spring6restmvc.services.BeerCsvServiceImpl;
 import jakarta.validation.ConstraintViolationException;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.context.annotation.Import;
 
 import java.math.BigDecimal;
+import java.util.List;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 
 @DataJpaTest
+@Import({BootStrapData.class, BeerCsvServiceImpl.class})
 class BeerRepositoryTest {
 
     @Autowired
     BeerRepository beerRepository;
 
+    @Test
+    void testGetBeerListByName() {
+        List<Beer> list = beerRepository.findAllByBeerNameIsLikeIgnoreCase("%IPA%");
+
+        assertThat(list.size()).isEqualTo(336);
+    }
 
     @Test
     void testSavedBeerNameTooLong() {
