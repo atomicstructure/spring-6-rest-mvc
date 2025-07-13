30f5cb1c436cd2be0514d960bccfe6ebf64f8a9b
Ogundare Oluwafemi <ogundarearnold@gmail.com>
2025-07-13 21:18:42 +0100

    Created beer order repository

---
+++ src/main/java/com/samantha/spring6restmvc/repositories/BeerOrderRepository.java
@@ -0,0 +1,11 @@
+package com.samantha.spring6restmvc.repositories;
+
+import com.samantha.spring6restmvc.entities.Beer;
+import com.samantha.spring6restmvc.entities.BeerOrder;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.UUID;
+
+public interface BeerOrderRepository extends JpaRepository<BeerOrder, UUID> {
+
+}
---
+++ src/test/java/com/samantha/spring6restmvc/repositories/BeerOrderRepositoryTest.java
@@ -0,0 +1,42 @@
+package com.samantha.spring6restmvc.repositories;
+
+import com.samantha.spring6restmvc.entities.Beer;
+import com.samantha.spring6restmvc.entities.Customer;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+
+
+
+@SpringBootTest
+class BeerOrderRepositoryTest {
+
+    @Autowired
+    BeerOrderRepository beerOrderRepository;
+
+    @Autowired
+    CustomerRepository customerRepository;
+
+    @Autowired
+    BeerRepository beerRepository;
+
+    Customer testCustomer;
+    Beer testBeer;
+
+    @BeforeEach
+    void setUp() {
+        testCustomer = customerRepository.findAll().getFirst();
+        testBeer = beerRepository.findAll().getFirst();
+    }
+
+    @Test
+    void testBeerOrders(){
+        System.out.println(beerOrderRepository.count());
+        System.out.println(customerRepository.count());
+        System.out.println(beerRepository.count());
+        System.out.println(testCustomer.getCustomerName());
+        System.out.println(testBeer.getBeerName());
+    }
+}
