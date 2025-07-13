4f6bbfc074e10ccd8555d1204f1cda78fa8d2bbb
Ogundare Oluwafemi <ogundarearnold@gmail.com>
2025-07-13 16:42:20 +0100

    V3 order tables customer_id changed to char instead of varchar

--- src/main/resources/db/migration/V3__add-order-tables.sql
+++ src/main/resources/db/migration/V3__add-order-tables.sql
@@ -8,7 +8,7 @@ CREATE TABLE `beer_order`
     customer_ref       varchar(255) DEFAULT NULL,
     last_modified_date datetime(6)  DEFAULT NULL,
     version            bigint       DEFAULT NULL,
-    customer_id        varchar(36)  DEFAULT NULL,
+    customer_id        char(36)  DEFAULT NULL,
     PRIMARY KEY (id),
     CONSTRAINT FOREIGN KEY (customer_id) REFERENCES customer (id)
 ) ENGINE = InnoDB;
