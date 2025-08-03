6f09822a1373c9742bf8739a62b1b3526570487d
Ogundare Oluwafemi <ogundarearnold@gmail.com>
2025-08-03 08:18:25 +0100

    Tested with postman but haven't still gotten the Access Token

--- src/main/java/com/samantha/spring6restmvc/config/SpringSecConfig.java
+++ src/main/java/com/samantha/spring6restmvc/config/SpringSecConfig.java
@@ -12,12 +12,13 @@ public class SpringSecConfig {
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http.authorizeHttpRequests((authz) -> authz
-                        .anyRequest().authenticated()
-                )
-                .oauth2ResourceServer((oauth2) -> oauth2
-                        .jwt(Customizer.withDefaults())
-                );
+        http.authorizeHttpRequests(authorize -> {
+                    authorize.anyRequest().authenticated();
+                })
+                .oauth2ResourceServer(httpSecurityOAuth2ResourceServerConfigurer -> {
+                    httpSecurityOAuth2ResourceServerConfigurer.jwt(Customizer.withDefaults());
+                });
+
         return http.build();
     }
 }
