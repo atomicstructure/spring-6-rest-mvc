f56f0a8360bd085868b8a5b7f04d3c95ce6b4ccd
Ogundare Oluwafemi <ogundarearnold@gmail.com>
2025-08-03 09:29:07 +0100

    Tested with spring mock mvc with jwt

--- src/test/java/com/samantha/spring6restmvc/controller/BeerControllerTest.java
+++ src/test/java/com/samantha/spring6restmvc/controller/BeerControllerTest.java
@@ -17,6 +17,7 @@ import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
 
+import java.time.Instant;
 import java.util.Optional;
 import java.util.UUID;
 
@@ -26,6 +27,7 @@ import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.BDDMockito.given;
 import static org.mockito.Mockito.verify;
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.httpBasic;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.jwt;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
@@ -64,7 +66,12 @@ class BeerControllerTest {
         given(beerService.updateBeerById(any(), any())).willReturn(Optional.of(beer));
 
         mockMvc.perform(patch(BeerController.BEER_ID_PATH, beer.getId())
-                        .with(httpBasic(USERNAME, PASSWORD))
+                        .with(jwt().jwt((jwt) -> jwt.claims(claims -> {
+                            claims.put("scope", "message.read");
+                            claims.put("scope", "message.write");
+                        })
+                                .subject("messaging-client")
+                                .notBefore(Instant.now().minusSeconds(5))))
                 .accept(MediaType.APPLICATION_JSON)
                 .contentType(MediaType.APPLICATION_JSON)
                 .content(objectMapper.writeValueAsString(beer)))
