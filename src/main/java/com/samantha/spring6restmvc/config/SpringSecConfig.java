9b946f4a562b062322b8d606c15f1bdbd9ca2702
Ogundare Oluwafemi <ogundarearnold@gmail.com>
2025-10-05 09:30:22 +0100

    Added the Spring Boot Actuator Dependency in the pom.xml and then added the configuration in the SpringSecConfig file in order.

--- src/main/java/com/samantha/spring6restmvc/config/SpringSecConfig.java
+++ src/main/java/com/samantha/spring6restmvc/config/SpringSecConfig.java
@@ -3,6 +3,7 @@ package com.samantha.spring6restmvc.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
@@ -11,6 +12,19 @@ import org.springframework.security.web.SecurityFilterChain;
 public class SpringSecConfig {
 
     @Bean
+    @Order(1)
+    public SecurityFilterChain actuatorSecurityFilterChain(HttpSecurity http) throws Exception {
+        http.securityMatcher("/actuator/**")
+                .authorizeHttpRequests(authorize -> {
+                    authorize.requestMatchers("/actuator/health", "/actuator/info").permitAll();
+                })
+                .httpBasic(Customizer.withDefaults());
+        return http.build();
+    }
+
+
+    @Bean
+    @Order(2)
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http.authorizeHttpRequests(authorize -> {
                     authorize.requestMatchers("/v3/api-docs**", "/swagger-ui/**", "/v3/api-docs/**",  "/swagger-ui.html").permitAll()
